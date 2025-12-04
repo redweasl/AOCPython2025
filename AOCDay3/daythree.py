046c6e6748312ea5e6469ee6f7c5ea96501446fb
adjohnson23 <johnso23@rose-hulman.edu>
2025-12-03 21:01:02 -0800

    Finish Day 3 Part 2

--- AOCDay3/daythree.py
+++ AOCDay3/daythree.py
@@ -3,25 +3,44 @@
 # In each bank, turn on two batteries, voltage = digits of number formed
 # Find largest possible joltage each bank can produce
 
-def total_joltage(banks):
+def total_joltage(banks, num_activate=2):
     sum = 0
     for bank in banks:
-        sum += max_joltage(bank)
+        sum += max_joltage(bank, num_activate)
     return sum
 
-def max_joltage(batteries):
-    joltageTen = 0
-    b2 = 0
-    for b1 in range (len(batteries) - 1):
-        if int(batteries[b1]) * 10 > joltageTen:
-            joltageTen = int(batteries[b1]) * 10
-            b2 = b1
-    b2 += 1
-    joltage = joltageTen
-    while b2 < len(batteries):
-        joltage = max(joltage, joltageTen + int(batteries[b2]))
-        b2 += 1
-    # print("Max joltage for a bank of %s was %d" % (batteries, joltage))
+def max_joltage(batteries, num_activate):
+    # joltageTen = 0
+    # b2 = 0
+    # for b1 in range (len(batteries) - 1):
+    #     if int(batteries[b1]) * 10 > joltageTen:
+    #         joltageTen = int(batteries[b1]) * 10
+    #         b2 = b1
+    # b2 += 1
+    # joltage = joltageTen
+    # while b2 < len(batteries):
+    #     joltage = max(joltage, joltageTen + int(batteries[b2]))
+    #     b2 += 1
+    oldJoltage = 0
+    joltage = 0
+    leftEnd = 0
+    max_idx = 0
+    rightEnd = len(batteries) - num_activate + 1
+    for _ in range (num_activate):
+        b = leftEnd
+        oldJoltage = joltage * 10
+        while b < rightEnd:
+            num = int(batteries[b])
+            if oldJoltage + num > joltage:
+                joltage = oldJoltage + num
+                max_idx = b
+                if num == 9:
+                    break
+            b += 1
+        leftEnd = max_idx + 1
+        rightEnd += 1
+
+    # print("Max joltage for a bank of %s turning on %d batteries was %d" % (batteries, num_activate, joltage))
     return joltage
 
 # Process input, returns a list of ranges
@@ -30,11 +49,18 @@ def process_input(input_name):
         content = file.read()
         return content.split("\n")
     
-# Part 1: Maximum total joltage
+# Part 1: Maximum total joltage (17316)
 def part_one():
     file = "Inputs/AOCday3.txt"
     banks = process_input(file)
-    print("Total joltage: %d" % (total_joltage(banks)))
+    print("Part 1 total joltage: %d" % (total_joltage(banks)))
+
+# Part 2: Instead of two batteries, it is now twelve
+def part_two():
+    file = "Inputs/AOCday3.txt"
+    banks = process_input(file)
+    print("Part 2 total joltage: %d" % (total_joltage(banks, 12)))
 
 part_one()
+part_two()
 
