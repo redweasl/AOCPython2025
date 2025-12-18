015b3e668836d11243909f95b24ce2b95a3cf058
adjohnson23 <johnso23@rose-hulman.edu>
2025-12-17 21:46:02 -0800

    Finish Day 10 Part 2

--- AOCDay10/dayten.py
+++ AOCDay10/dayten.py
@@ -4,7 +4,8 @@
 # Light diagram: What the final button layout should be
 # Wiring schematic: For a button, what lights are toggled
 
-import time
+import time, z3
+from z3 import Int, Solver
 
 class Machine:
     def __init__(self, ld: list, bschems: list, jreqs: list):
@@ -78,35 +79,43 @@ print(f"Part 1 took {elapsed//1_000_000} ms")
 # Each button press increases it by a jolt
 # Fewest total button presses to meet joltage reqs
 def fewest_button_presses_jolts(machine: Machine):
-    cv = list(0 for i in range(len(machine.ld)))
-    str_cv = str(cv)
-    dict = set()
-    dict.add(str_cv[1:len(str_cv)-1])
-    queue = []
-    queue.append([cv, 0])
-    print(f"Dictionary contains {dict}")
+    # Use system of equations and z3-solver to solve
+    # Create variables
+    vars = []
+    for i in range(len(machine.bschems)):
+        vars.append(Int(f"v{i}"))
+    
+    # Create equations
+    s = Solver()
+    for i in range(len(vars)):
+        s.add(vars[i] >= 0)
 
-    while len(queue) > 0:
-        diagram, num_presses = queue.pop(0)
-        print(num_presses)
-        for bschem in machine.bschems:
-            new_diagram = diagram.copy()
-            valid = True
-            for num in bschem:
-                new_diagram[num] += 1
-                if new_diagram[num] > machine.jreqs[num]:
-                    valid = False
-            # print(f"Config after pressing a button: {new_diagram} is it valid? {valid}")
-            if new_diagram == machine.jreqs:
-                print(f"JOLT: Fewest button presses for machine {machine} is {num_presses + 1}")
-                return num_presses + 1
-            str_diagram = str(new_diagram)
-            str_diagram = str_diagram[1:len(str_diagram)-1]
-            if valid and str_diagram not in dict:
-                dict.add(str_diagram)
-                queue.append([new_diagram, num_presses + 1])
-    print("Found no valid sequences")
-    return -1
+    for i in range(len(machine.jreqs)):
+        sum = machine.jreqs[i]
+        equation = None
+        for j in range(len(machine.bschems)):
+            if i in machine.bschems[j]:
+                equation = vars[j] if equation is None else equation + vars[j]
+        s.add(equation == sum)
+
+    # Solve the constraints until no solutions remain: return the minimum sum
+    # Used this resource to learn how to manage multiple solutions: https://brandonrozek.com/blog/obtaining-multiple-solutions-z3/
+    min_sum = None
+    result = s.check()
+    while result == z3.sat:
+        m = s.model()
+        sum = 0
+        for var in m.decls():
+            sum += m[var].py_value()
+        min_sum = sum if min_sum is None else min(min_sum, sum)
+
+        block = []
+        for var in m:
+            block.append(var() != m[var])
+        s.add(z3.Or(block))
+        result = s.check()
+
+    return min_sum
 
 def presses_sum_jolts(machines: list[Machine]):
     sum = 0
@@ -118,14 +127,9 @@ def part_two():
     file = "Inputs/AOCday10.txt"
     machines = process_input(file)
     sum = presses_sum_jolts(machines)
-    print(f"Part 1 (UNKNOWN): The fewest sum of presses is {sum}")
+    print(f"Part 2 (14677): The fewest sum of presses is {sum}")
 
 before = time.perf_counter_ns()
 part_two()
 elapsed = time.perf_counter_ns() - before
 print(f"Part 2 took {elapsed//1_000_000} ms")
-
-file = "AOCDay10/AOCday10example.txt"
-machines = process_input(file)
-sum = presses_sum_jolts(machines)
-print(f"Part 2 example: The fewest sum of presses is {sum}")
